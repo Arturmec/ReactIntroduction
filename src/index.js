cfe4d50f4f51c924ed086b2bb921c03fdf0ba71e
Artur Della Favera <arturdellafavera@gmail.com>
2020-03-05 08:34:33 -0300

    FEATURE / Rederiza o componente e envia para o html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const soma = (a, b) => a + b;
-
-alert(soma(1, 5));
+import React from 'react';
+/** React-DOM faz integração do react com o browser */
+import { render } from 'react-dom';
+import App from './App';
+/** 
+* Passa, como primeiro parametro, App como tag do html
+* Passa onde o conteúdo será jogado
+*/
+render(<App />, document.getElementById('app'));
