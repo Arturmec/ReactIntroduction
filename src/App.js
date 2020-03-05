706afb0a726860df2ed99215aa793af6b720d401
Artur Della Favera <arturdellafavera@gmail.com>
2020-03-05 09:53:19 -0300

    FEATURE / Componente App retorna tag techlist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import ironman from './assets/ironman.jpg'
+import TechList from './components/TechList';
 /** Maneira mais fácil de criar um componente é através de uma function */
 function App() {
   /** 
-  * O react permite que exista código html dentro do javascript 
-  * width é o tamanho que a imagem que aparecerá na tela
+  * Salva TechList como uma tag do html
   */
-  return <img src={ironman} width="200"/>
+  return <TechList />
 }
 
 export default App;
